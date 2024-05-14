ecd85f7df48447d7d85c5b35c6ad532277e3d0b0
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2024-05-14 16:00:55 -0400

    changes to card and dynamic images

--- src/productCard.js
+++ src/productCard.js
@@ -1,3 +1,34 @@
+
+let projects = [
+  {
+    name: "Insights Builder",
+    company: "Foursquare",
+    image: "dormdesign1.png",
+    imageClass:"single"
+},
+{
+  name: "Projects Portal",
+  company: "Foursquare",
+  image: "dormdesign1.png",
+  imageClass:"single"
+},
+{
+  name: "Lookout",
+  company: "Fathom",
+  image: "dormdesign2.png",
+  secondImage:"dormdesign1.png",
+  imageClass:"double",
+  secondImageClass:"double-bg"
+},
+{
+  name: "Banyan",
+  company: "Fathom",
+  image: "dormdesign1.png",
+  imageClass:"single"
+}
+
+]
+
 const portal = {
     name: "Projects Portal",
     company: "Foursquare",
@@ -9,9 +40,12 @@ function makeCard(project) {
 
 const projectCard = document.createElement('template');
 
+if(project.imageClass == "single"){
+
 projectCard.innerHTML = `
 	<div class="projectCard bg-white mt-8">
-	<div class="top-card h-64 bg-gray-100">
+	<div class="top-card h-80 bg-gray-100 overflow-hidden flex justify-center relative items-center">
+  <img class="${project.imageClass} rounded shadow-image" src="../images/${project.image}">
 	</div>
 	<div class="p-4">
   <p class="text-base text-gray-400">${project.company}</p>
@@ -19,11 +53,31 @@ projectCard.innerHTML = `
   </div>
   </div>
 `;
+}
+else {
+  projectCard.innerHTML = `
+	<div class="projectCard bg-white mt-8">
+	<div class="top-card h-80 bg-gray-100 overflow-hidden flex justify-center relative items-center">
+  <img class="${project.secondImageClass} rounded shadow-image" src="../images/${project.secondImage}">
+  <img class="${project.imageClass} rounded shadow-image" src="../images/${project.image}">
+	</div>
+	<div class="p-4">
+  <p class="text-base text-gray-400">${project.company}</p>
+   <h1 class="font-medium text-xl black">${project.name}</h1>
+  </div>
+  </div>
+`;
+}
 
 document.querySelector('.projects-container').appendChild(projectCard.content);
 
 }
 
-makeCard(portal);
-makeCard(portal);
-makeCard(portal);
+projects.forEach((project) => 
+  makeCard(project)
+);
+
+
+// makeCard(portal);
+// makeCard(portal);
+// makeCard(portal);
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        'image': '0px 0px 24px -4px rgba(0, 0, 0, 0.3)',
+      }
+    },
   },
   plugins: [],
 }
+
+
 
