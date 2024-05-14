59571fd171b4f3d358e4438e1612c7740ac9b7d0
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2024-05-14 14:17:45 -0400

    changes

---
+++ src/productCard.js
@@ -0,0 +1,29 @@
+const portal = {
+    name: "Projects Portal",
+    company: "Foursquare",
+    image: "none"
+};
+
+
+function makeCard(project) {
+
+const projectCard = document.createElement('template');
+
+projectCard.innerHTML = `
+	<div class="projectCard bg-white mt-8">
+	<div class="top-card h-64 bg-gray-100)">
+	</div>
+	<div class="p-4">
+  <p class="text(base gray-400)">${project.company}</p>
+   <h1 class="font-medium text(xl black)">${project.name}</h1>
+  </div>
+  </div>
+`;
+
+document.querySelector('.projects-container').appendChild(projectCard.content);
+
+}
+
+makeCard(portal);
+makeCard(portal);
+makeCard(portal);
