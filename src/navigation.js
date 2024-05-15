cba2e4ec29fbf4ef6ceeff939024c471e921673e
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2024-05-15 00:26:10 -0400

    new

--- src/navigation.js
+++ src/navigation.js
@@ -16,6 +16,30 @@ function makeButton(project) {
       </div>
       </button>
       </div>
+      <div class="fixed left-4 bottom-4 flex flex-col gap-y-2">
+        <a>
+        <div class="w-10 h-10 flex justify-center items-center text-base font-medium text-red-700 hover:bg-red-100">
+            LI
+            </div>
+        </a>
+        <a>
+        <div class="w-10 h-10 flex justify-center items-center text-base font-medium text-red-700 hover:bg-red-100">
+            CV
+            </div>
+        </a>
+        <a>
+        <div class="w-10 h-10 flex justify-center items-center text-base font-medium text-red-700 hover:bg-red-100">
+        <span class="material-symbols-outlined">
+        mail
+      </span>
+            </div>
+        </a>
+      </div>
+
+      <div class="fixed left-4 top-4 header-text mix-blend-multiply text-red-700 text-3xl">
+      koby moreno
+      </div>
+
     `;
     
     document.body.appendChild(navButton.content);
