b926acc9b4699a8af6b0fc85b7f6380684c499ec
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2024-05-15 15:49:58 -0400

    ok

--- navigation.js
+++ navigation.js
@@ -1,7 +1,50 @@
 
+let pageId = `${document.body.getAttribute('id')}`;
+console.log(pageId);
+
+let links = [
+  "https://www.linkedin.com/in/koby-moreno-096988a1/",
+  "https://drive.google.com/file/d/1eQKKu3h04G8tEOIo23trmIUpX-gZswWa/view?usp=sharing",
+  "mailto:kobymoreno@gmail.com"
+];
+
+function primaryBtn(name, destination) {
+  return `<a href="${destination}"><button><div class="px-4 py-2 bg-red-700 hover:bg-red-800 text-base text-white">${name}</div></button></a>`
+};
+
+function secondaryBtn(name, destination) {
+  return `<a href="${destination}"><button><div class="shadow-button shadow-red-700 px-4 py-2 hover:bg-red-100 text-base text-red-700">${name}</div></button></a>`
+};
 
 
-let primaryBtn ='<button><div class="px-4 py-2 bg-red-700 hover:bg-red-800 text-base text-white">Work</div></button>';
+function generateBtn1() {
+
+  if (pageId == "about"){
+    return secondaryBtn("Work","index.html");
+  }
+  else {
+    return primaryBtn("Work","index.html");
+  }
+};
+
+function generateBtn2() {
+
+  if (pageId == "about"){
+    return primaryBtn("About","about.html");
+  }
+  else {
+    return secondaryBtn("About","about.html");
+  }
+};
+
+let titleName = document.querySelector('title').innerHTML;
+console.log(titleName);
+
+let email = '<span class="material-symbols-outlined">mail</span>';
+
+function tertiaryButton(text, link) {
+  return `<a href="${link}"><div class="w-10 h-10 flex justify-center items-center text-base font-medium text-red-700 hover:bg-red-100">${text}</div></a>`
+};
 
 function makeButton(project) {
 
@@ -10,34 +53,16 @@ function makeButton(project) {
     navButton.innerHTML = `
     <div class="fixed mix-blend-multiply top-4 flex gap-x-2 justify-between margins-x w-screen">
     <div class="header-text mix-blend-multiply text-red-700 text-3xl">
-    koby moreno
+    ${titleName.toLowerCase()}
     </div>
     <div class="flex gap-x-2">
-    ${primaryBtn}
-      <button>
-        <div class="shadow-button shadow-red-700 px-4 py-2 hover:bg-red-100 text-base text-red-700">
-        About
-      </div>
-      </button>
+${generateBtn1()}
+${generateBtn2()}
       </div>
       <div class="fixed left-4 bottom-4 flex flex-col gap-y-2">
-        <a>
-        <div class="w-10 h-10 flex justify-center items-center text-base font-medium text-red-700 hover:bg-red-100">
-            LI
-            </div>
-        </a>
-        <a>
-        <div class="w-10 h-10 flex justify-center items-center text-base font-medium text-red-700 hover:bg-red-100">
-            CV
-            </div>
-        </a>
-        <a>
-        <div class="w-10 h-10 flex justify-center items-center text-base font-medium text-red-700 hover:bg-red-100">
-        <span class="material-symbols-outlined">
-        mail
-      </span>
-            </div>
-        </a>
+        ${tertiaryButton('LI', links[0])}
+        ${tertiaryButton('CV', links[1])}
+        ${tertiaryButton(email, links[2])}
       </div>
       </div>
 
