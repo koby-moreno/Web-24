eb512b831c1967a8436d8a8bf3cea187b30b4675
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2026-01-04 23:40:35 -0500

    Update productCard.js

--- productCard.js
+++ productCard.js
@@ -15,15 +15,6 @@ let projects = [
     imageClass:"single",
     destination:"insights.html"
 },
-// {
-//   name: "Table Component",
-//   company: "Foursquare",
-//   image: "projects3.png",
-//   secondImage:"projects1.png",
-//   imageClass:"double",
-//   secondImageClass:"double-bg",
-//   destination:"table.html"
-// },
 {
   name: "Projects Portal",
   company: "Foursquare",
