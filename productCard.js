432a4455d0ec6a77d29f6d854d54dbffaa5d5a80
koby-moreno <70607733+koby-moreno@users.noreply.github.com>
2026-01-04 23:51:19 -0500

    new new

--- productCard.js
+++ productCard.js
@@ -66,7 +66,7 @@ if(project.imageClass == "single" || project.imageClass == "noShadow"){
 projectCard.innerHTML = `
 <a href="${project.destination}" class="border border-transparent hover:border hover:border-gray-200  dark:hover:shadow-image-dm hover:shadow-image-light hover:cursor-pointer dark:hover:border-dark-3 dark:bg-dark-1 bg-white projectCard mt-8">
 	<div class="top-card aspect-ratio-short dark:bg-dark-2 bg-gray-100 overflow-hidden flex justify-center relative items-center">
-  <img class="${project.imageClass} rounded shadow-image" src="images/${project.image}">
+  <img class="${project.imageClass} rounded shadow-image" src="./images/${project.image}">
 	</div>
 	<div class="p-4">
   <p class="dark:text-zinc-400 text-base text-gray-400">${project.company}</p>
@@ -80,7 +80,7 @@ else {
   <a href="${project.destination}" class="border border-transparent hover:border dark:hover:shadow-image-dm hover:shadow-image-light hover:cursor-pointer  hover:border-gray-200 dark:hover:border-dark-3 dark:bg-dark-1 bg-white projectCard mt-8">
 	<div class="top-card aspect-ratio-short dark:bg-dark-2 bg-gray-100 overflow-hidden flex justify-center relative items-center">
   <img class="${project.secondImageClass} rounded shadow-image" src="images/${project.secondImage}">
-  <img class="${project.imageClass} rounded shadow-image" src="images/${project.image}">
+  <img class="${project.imageClass} rounded shadow-image" src="./images/${project.image}">
 	</div>
 	<div class="p-4">
   <p class="dark:text-zinc-400 text-base text-gray-400">${project.company}</p>
